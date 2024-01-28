ae13b2a21427179c736f7d8c809f5ac47fd7bb5b
Grayton Ward <graytonw@jfrog.com>
2024-01-28 12:11:17 -0500

    feat: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        }
+    }),
+    output: 'server',
+    site: 'https://graytonward.com',
     integrations: [
         mdx(),
         sitemap(),
